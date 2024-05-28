5b68fc8f5091b9ff11f2fed8f0c3a32f609d8de0
iam-raja <rajapeta45@gmail.com>
2024-05-28 10:45:38 +0530

    demo

--- output.tf
+++ output.tf
@@ -2,19 +2,19 @@ output "vpc_id" {
     value = aws_vpc.main.id
    }
     output "public_subnet_ids" {
-    value=aws_subnet.public.id   
+    value=aws_subnet.public[*].id   
    }
 
      output "private_subnet_ids" {
-    value=aws_subnet.private.id  
+    value=aws_subnet.private[*].id  
    }
 
      output "database_subnet_ids" {
-    value=aws_subnet.database.id
+    value=aws_subnet.database[*].id
      }
 
      output "database_subnet_groups" {
-       value=aws.aws_db_subnet_group.id
+       value=aws.aws_db_subnet_group.default.id
      }
 
      output "igw_id" {
